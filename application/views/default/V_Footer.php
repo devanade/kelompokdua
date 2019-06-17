67ebf8ec99635b1c3cd0460b1b2c49c280f000b7
devanade <devanaade@gmail.com>
2019-06-17 18:10:57 +0700

    menambahkan header dan footer

---
+++ application/views/default/V_Footer.php
@@ -0,0 +1,4 @@
+
+        <footer class="footer">
+          <div class="container-fluid clearfix">
+            <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright &copy; <?=date('Y');?> DevTeam
